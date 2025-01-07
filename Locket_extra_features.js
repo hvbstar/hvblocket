1f25b78a4b02b7e20207c6382cbd311fddd49155
Hoàng Văn Bảo <baonet003@gmail.com>
2025-01-07 14:24:28 +0700

    Update Locket_extra_features.js

--- Locket_extra_features.js
+++ Locket_extra_features.js
@@ -1,14 +1,10 @@
-// Locket_extra_features.js - Unlock Video 15s & Badge
 let obj = JSON.parse($response.body);
 
-// Enable Locket badge (gold heart)
+// Bật huy hiệu Locket (trái tim vàng)
 if (!obj.features) obj.features = {};
 obj.features.badge = true;
 
-// Enable 15s video recording
+// Bật quay video 15 giây
 obj.features.video_duration = 15;
-
-// Log the modified features for debugging
-console.log("Modified Features:", JSON.stringify(obj, null, 2));
 
 $done({ body: JSON.stringify(obj) });
