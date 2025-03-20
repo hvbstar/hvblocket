b02d9623af024e9d89ae8c8ffaf89b306098145a
Hoàng Văn Bảo <baonet003@gmail.com>
2025-03-20 14:34:11 +0700

    Update deleteHeader.js

--- deleteHeader.js
+++ deleteHeader.js
@@ -1,6 +1,6 @@
 // Updated deleteHeader.js
 // ========= Header Modification ========= //
-const version = 'V1.0.3';
+const version = 'V1.0.4';
 
 function setHeaderValue(e, a, d) {
   var r = a.toLowerCase();
@@ -12,6 +12,7 @@ var modifiedHeaders = $request.headers;
 
 // Thay đổi giá trị của X-RevenueCat-ETag
 setHeaderValue(modifiedHeaders, "X-RevenueCat-ETag", "");
+setHeaderValue(modifiedHeaders, "X-Locket-Audio", "enabled"); // Thử bật âm thanh
 
 // Debug: In header đã sửa (tuỳ chọn)
 console.log("Modified Headers:", JSON.stringify(modifiedHeaders));
